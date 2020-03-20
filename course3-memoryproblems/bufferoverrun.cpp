89f800673387cf8e7f618d1495983bddff73b76d
Robotawi <mohamed.s.raessa@ieee.org>
2020-03-20 18:45:08 +0900

    Reproduce buffer overrun and memory leak

---
+++ course3-memoryproblems/bufferoverrun.cpp
@@ -0,0 +1,9 @@
+//
+// Created by zozo on 3/8/20.
+//
+#include <cstring>
+int main(){
+    char c[5];
+    strcpy(c, "I am into cpp");
+
+}
---
+++ course3-memoryproblems/memory_leak.cpp
@@ -0,0 +1,7 @@
+//
+// Created by zozo on 3/8/20.
+//
+
+int main(){
+    int* ptr = new int[10];
+}
